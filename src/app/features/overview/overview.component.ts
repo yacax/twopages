b861411e71e12becafbe455a4a9222094ad53a6a
yacax <yacax@yandex.ru>
2024-04-17 12:27:12 +0300

    Set routing config and create files structural.

---
+++ src/app/features/overview/overview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss'],
+  standalone: true,
+})
+export class OverviewComponent {}
